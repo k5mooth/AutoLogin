e68ee0ef9a1005d0687776c4caab038e83a8fff1
Karalius Smooth <karalius.smooth@gmail.com>
2017-06-06 12:40:51 -0500

    completed main and file format checking

--- logmein.py
+++ logmein.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import Login
 import argparse
 import sys
@@ -15,7 +17,7 @@ import re
 
 # ( (-t, -f, -g, -y), -a )* ; ((!username,!password), -i)*
 def msg():
-    return ""
+    return "usage: %s [-h] [ [-a] | [-t] [-f] [-g] [-y] ] ( [-i FILE ] | username password )" % ( sys.argv[0] )
 
 def getArgs():
     parseit = argparse.ArgumentParser(description="Log into any of FB, Twitter, Gmail, and Yahoo using <username> and <password>",usage=msg())
@@ -25,35 +27,83 @@ def getArgs():
     parseit.add_argument('-f','--facebook', action='store_true',help="Log into Facebook")
     parseit.add_argument('-y','--yahoo', action='store_true',help="Log into Yahoo")
     parseit.add_argument('-g','--gmail', action='store_true',help="Log into Gmail")
-    parseit.add_argument('-i','--file', type=file, dest="input_file",help="Log in with credentials in file with format as 'username:password'")
+    parseit.add_argument('-i','--file', dest="input_file",help="Log in with credentials in file with format as 'username:password'")
     parseit.add_argument("username",type=str,nargs='?',default="",help="Username to log in as")
     parseit.add_argument("password",type=str,nargs='?',default="",help="Password to log in with")
     
     args = parseit.parse_args()
     site_opt = (args.twitter or args.facebook or args.yahoo or args.gmail)
 
-    if args.all and site_opt:
+    if args.all and site_opt: # -a used with other site opts
         print msg()
         print "%s: error: argument -a/--all: not allowed with the arguments: -t/--twitter -f/--facebook -y/--yahoo -g/--gmail" % (sys.argv[0])
-        return -1
+        return None
 
     if (args.input_file is not None): # input file added
-        if (args.username != "" or args.password != ""):
+        if (args.username != "" or args.password != ""): # if -i used with username and password args
             print msg()
             print "%s: error: argument -i/--file: not allowed with the arguments: username password" % (sys.argv[0])
-            return -1
+            return None
     else: # no input file
-        if (args.username == "" or args.password == ""): # if no user and pass
+        if (args.username == "" or args.password == ""): # if no "-i FILE" or "username and password"
             print msg()
             print "%s: error: the following arguments are required: username password" % (sys.argv[0])
-            return -1
+            return None
 
     return args
 
+def check_fformat(infile):
+    r = re.compile('^.*:.*$')
+    f = open(infile,'r')
+    
+    numlines = sum(1 for line in f)
+    f.seek(0)
+
+    for count,line in enumerate(f):
+        if (line.count(':') == 1) and (r.match(line) is None):
+            print "%s: error: expects format 'user:pass': Line %d " % (sys.argv[0],count+1)
+            return -1
+    
+    f.close()
+    return numlines
+
+def log_into(args, client):
+    
+
 def main():
-     
-    #login_client = Login.Login(username='',password='')
-    #site.login(logsite)
+    max_lines = 0
+    creds = []
+    f = None
+
+    args = getArgs()
+    login_client = Login.Login()
+    
+    if (args is None):
+        return -1
+    
+    if (args.input_file is not None):
+        max_lines = check_fformat(args.input_file)
+        if max_lines == -1: 
+            return -1
+        
+    f = open(args.input_file)
+    uname = args.username
+    pword = args.password
+
+    while (args.input_file is not None) or (uname != "" and pword != "") :
+        if (uname != "" and pword != ""): 
+            log_into(args, login_client)
+            break
+        try:
+            line = f.readline().split(":")
+        except (EOFerror):
+            break
+        
+        args.username = line[0]
+        args.password = line[1]
+        log_into(args,login_client)
+
+    f.close()
 
 if __name__ == "__main__":
-	main()
+    main()
